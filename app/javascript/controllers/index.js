047131c2c3e003b163f1e8866ca920b01d0bc8ac
taiks-623 <199321893+taiks-623@users.noreply.github.com>
2026-01-06 13:35:25 +0900

    [add] ローディング中だとわかるように表示

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,7 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
