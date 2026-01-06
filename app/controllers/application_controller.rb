842d9555c2c27b8acf46818f83374ebba733dea5
taiks-623 <199321893+taiks-623@users.noreply.github.com>
2026-01-06 15:06:51 +0900

    [add] ログインページ見た目整形

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Deviseのログイン後のリダイレクト先を設定
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  # Deviseのサインアップ後のリダイレクト先を設定
+  def after_sign_up_path_for(resource)
+    dashboard_path
+  end
 end
